85e59b8240db89a04e346cef4fa48ca8809301f6
Jordan Johnson <jkj86@hotmail.com>
2017-10-01 08:44:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const input = document.querySelector('input')
+   
+  input.addEventListener('keydown', function(e) {
+    if (e.which === 71) {
+      return e.preventDefault()
+    }
+  })
 }
