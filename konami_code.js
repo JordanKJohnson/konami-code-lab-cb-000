93a66e75e12d4b855c58f81933eb08c05939282c
Jordan Johnson <jkj86@hotmail.com>
2017-10-01 08:47:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,4 +9,24 @@ function init() {
       return e.preventDefault()
     }
   })
+
+  // Keep track of index outside of the event handler.
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 }
